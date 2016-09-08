4bb3dda899af2d5e6eac51d597af16a43259711c
tianheng <zth198814@gmail.com>
2016-09-08 09:20:51 -0700

    working on virtual list page

--- app/scripts/panel/business/business.directive.js
+++ app/scripts/panel/business/business.directive.js
@@ -6,8 +6,15 @@
 angular.module('baoziApp')
     .directive('businessVirtualList', function () {
       return {
+        scope: {
+          businessDataProvider: '='
+        },
         restrict: 'E',
         require: 'ngModel',
-        templateUrl: 'templates/panel/business/business.virtual.list.html'
+        templateUrl: 'templates/panel/business/business.virtual.list.html',
+        link: function (scope, elem, attrs, ngModelCtrl) {
+          var rowHeight = 20;
+
+        }
       }
     });
