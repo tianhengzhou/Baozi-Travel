4f84e079a69a957898133b75649e4a6aa05430cb
tianhengzhou <zth198814@gmail.com>
2016-09-08 17:19:47 -0700

    Create a virtual list directive

--- app/scripts/panel/business/business.directive.js
+++ app/scripts/panel/business/business.directive.js
@@ -4,7 +4,7 @@
 
 "use strict";
 angular.module('baoziApp')
-    .directive('businessVirtualList', function () {
+    .directive('businessVirtualList',[ '$window' ,function ($window) {
       return {
         scope: {
           businessDataProvider: '='
@@ -14,7 +14,26 @@ angular.module('baoziApp')
         templateUrl: 'templates/panel/business/business.virtual.list.html',
         link: function (scope, elem, attrs, ngModelCtrl) {
           var rowHeight = 20;
-
+          scope.height = $window.innerHeight * 0.6;
+          scope.scrollTop = 0;
+          scope.visibleProvider = [];
+          scope.cellsPerPage = 0;
+          scope.numberofCells = 0;
+          scope.canvasHeight = 0;
+          scope.init = function () {
+            elem[0].addEventListener('scroll', scope.onScroll);
+            scope.cellsPerPage = Math.round(scope.height / rowHeight);
+            scope.numberofCells = 3 * scope.cellsPerPage;
+            scope.canvasHeight = {
+              height: ($window.innerHeight * 0.6) + 'px'
+            };
+            scope.updateList();
+          }
+          scope.updateList = function () {
+            var firstCell = Math.max(
+                Math.floor(scope.scrollTop / rowHeight) - scope.cellsPerPage, 0);
+            var cellsToCreate = Math.min(firstCell + scope.numberofCells, scope.numberofCells);
+          }
         }
       }
-    });
+    }]);
